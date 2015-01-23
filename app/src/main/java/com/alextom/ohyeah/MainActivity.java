9f87966e75c8570e77ab1ae91d4d31687004addf
Alex Fu <alex.fu914@gmail.com>
2015-01-23 13:05:16 -0500

    Update animation to a more complex one

--- app/src/main/java/com/alextom/ohyeah/MainActivity.java
+++ app/src/main/java/com/alextom/ohyeah/MainActivity.java
@@ -1,13 +1,24 @@
 package com.alextom.ohyeah;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageButton;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
@@ -22,6 +33,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
   private MediaPlayer mediaPlayer;
   private View root;
   private ImageButton button;
+  private AnimatorSet animatorSet;
 
   private int counter = 0;
 
@@ -61,12 +73,53 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
 
     root.setBackgroundResource(R.color.kool_aid_red);
     mediaPlayer.start();
-    button.startAnimation(AnimationUtils.loadAnimation(this, R.anim.spin));
-
-    if (counter == 3) {
-      findViewById(R.id.special).setVisibility(View.VISIBLE);
+    
+    if (animatorSet == null) {
+      animatorSet = createMainAnimatorSet();
     }
-    counter++;
+    
+    animatorSet.start();
+  }
+
+  private AnimatorSet createMainAnimatorSet() {
+    button.setPivotX(button.getWidth() / 2);
+    button.setPivotY(button.getHeight() / 2);
+    int animationTime = 500;
+    
+    // Base spin animation
+    final ValueAnimator spinAnimation = ObjectAnimator.ofFloat(button, "rotation", 0, 720);
+    spinAnimation
+        .setDuration(animationTime)
+        .setInterpolator(new AccelerateDecelerateInterpolator());
+    
+    // Scale up and down animation
+    AnimatorSet scaleAnimatorSet = new AnimatorSet();
+    final ValueAnimator scaleYAnimationUp = ObjectAnimator.ofFloat(button, "scaleY", 1, 2);
+    final ValueAnimator scaleXAnimationUp = ObjectAnimator.ofFloat(button, "scaleX", 1, 2);
+    scaleYAnimationUp
+        .setDuration(animationTime / 2)
+        .setInterpolator(new AccelerateInterpolator());
+    scaleXAnimationUp
+        .setDuration(animationTime / 2)
+        .setInterpolator(new AccelerateInterpolator());
+
+    final ValueAnimator scaleYAnimationDown = ObjectAnimator.ofFloat(button, "scaleY", 2, 1);
+    final ValueAnimator scaleXAnimationDown = ObjectAnimator.ofFloat(button, "scaleX", 2, 1);
+    scaleYAnimationDown
+        .setDuration(animationTime / 2)
+        .setInterpolator(new DecelerateInterpolator());
+    scaleXAnimationDown
+        .setDuration(animationTime / 2)
+        .setInterpolator(new DecelerateInterpolator());
+
+    scaleAnimatorSet.playTogether(scaleXAnimationUp, scaleYAnimationUp);
+    scaleAnimatorSet.play(scaleXAnimationDown).after(scaleXAnimationUp);
+    scaleAnimatorSet.play(scaleYAnimationDown).after(scaleYAnimationUp);
+
+    // Create the animation set
+    AnimatorSet set = new AnimatorSet();
+    set.playTogether(spinAnimation, scaleAnimatorSet);
+    return set;
   }
 
   @Override
